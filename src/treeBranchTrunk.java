8ba1f7f1faaa81a8a8a68e8086c1aaf771137d6f
VkumarStack <vksogi@gmail.com>
2020-03-09 13:16:26 -0700

    Initial commit

---
+++ src/treeBranchTrunk.java
@@ -0,0 +1,7 @@
+public class treeBranchTrunk
+{
+    public static void main (String[] args)
+    {
+        System.out.println("The trees are in bloom");
+    } // end main method
+} // end driver class
